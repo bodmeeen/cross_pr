636255c567b585510a3689dffea21ef7ad74322d
Maksym <maks.demianp@gmail.com>
2025-09-16 23:19:43 +0300

    Compiled into a native image

--- src/main/java/org/project_j/entity/AppUser.java
+++ src/main/java/org/project_j/entity/AppUser.java
@@ -1,13 +1,35 @@
-package org.project_j.entity;
+    package org.project_j.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+    import jakarta.persistence.*;
 
-@Entity
-@Table(name = "app_user")
-public class AppUser {
-    @Id
-    public Long id;
-    public String name;
-}
+    @Entity
+    @Table(name = "app_user")
+    public class AppUser {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private  Long id;
+
+        @Column(nullable = false, length = 100)
+        private String name;
+
+        public AppUser(){}
+        public AppUser(String name){
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
